0dc4be48113b104647474547b68769ba840c3c7e
kleinjr1 <kleinjr1@berkeley.edu>
2026-03-29 02:59:08 -0400

    edits to use right analysis_df

--- Model2_ffnn.py
+++ Model2_ffnn.py
@@ -11,9 +11,8 @@ import matplotlib.pyplot as plt
 import json
 
 # Parameters
-analysis_df_path = '/Users/kleinjr1/Downloads/analysis_df.csv' # output from eda_and_preprocessing ruchit branch
-output_params_path = '/Users/kleinjr1/Downloads/best_hps.json'
-output_fig_path = '/Users/kleinjr1/Downloads/results.png'
+analysis_df_path = '/Users/kleinjr1/Downloads/analysis_df2.csv' # output from eda_and_preprocessing ruchit branch
+output_params_path = '/Users/kleinjr1/Downloads/best_hps2.json'
 
 # Load df
 df = pd.read_csv(analysis_df_path)
@@ -31,7 +30,7 @@ X_train, X_val, Y_train, Y_val = train_test_split(X_temporary, Y_temporary, test
 ############
 
 # Scale features
-numeric_cols = ['PRECIPITATION', 'MAX_TEMP', 'MIN_TEMP', 'AVG_WIND_SPEED', 'LAGGED_PRECIPITATION', 'LAGGED_AVG_WIND_SPEED']
+numeric_cols = ['PRECIPITATION', 'MAX_TEMP', 'MIN_TEMP', 'AVG_WIND_SPEED', 'LAGGED_PRECIPITATION', 'LAGGED_AVG_WIND_SPEED', 'DAYS_WITHOUT_RAIN']
 numeric_cols_lower = [i.lower() for i in numeric_cols]
 scaler = StandardScaler()
 
@@ -75,62 +74,62 @@ tuner.search(X_train_std,
 best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
 with open(output_params_path, "w") as f:
     json.dump(best_hps.values, f, indent=2)
-
-# Build the model with the optimal hyperparameters
-model = tuner.hypermodel.build(best_hps)
-
-# Find best epoch
-history = model.fit(X_train_std,
-                    Y_train,
-                    batch_size=32,
-                    validation_data=(X_val_std, Y_val),
-                    class_weight={0: 1, 1: 2}, # Adjusting for the 67/33 split
-                    epochs=100)
-
-val_acc_per_epoch = history.history['val_binary_accuracy']
-best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
-print('Best epoch: %d' % (best_epoch,))
-
-# Show learned model with weights and biases from
-W_final_layer, b_final_layer = model.layers[-1].get_weights()
-
-train_loss = history.history['loss'][-1]
-train_binary_accuracy = history.history['binary_accuracy'][-1]
-val_loss = history.history['val_loss'][-1]
-val_binary_accuracy = history.history['val_binary_accuracy'][-1]
-
-print('Final train loss:', train_loss)
-print('Final validation loss:', val_loss)
-print('Final train_accuracy:', train_binary_accuracy)
-print("Final_val_accuracy:", val_binary_accuracy)
-
-percentage_diff = ((train_loss - val_loss)/train_loss)*100
-print(" percentage difference between the losses observed on the training and validation datasets:",percentage_diff )
-
-# grab history
-history = history.history
-
-# plot loss for train and validation
-fig = plt.figure(figsize=(16, 4))
-ax = fig.add_subplot(1, 2, 1)
-plt.plot(history['loss'], lw=2, color='darkgoldenrod')
-plt.plot(history['val_loss'], lw=2, color='indianred')
-plt.legend(['Train', 'Validation'], fontsize=10)
-ax.set_xlabel('Epochs', size=10)
-ax.set_ylabel('Loss')
-ax.set_title('Loss for train and validation');
-
-# plot accuracy for train and validation
-ax = fig.add_subplot(1, 2, 2)
-plt.plot(history['binary_accuracy'], lw=2, color='darkgoldenrod')
-plt.plot(history['val_binary_accuracy'], lw=2, color='indianred')
-plt.legend(['Train', 'Validation'], fontsize=10)
-ax.set_xlabel('Epochs', size=10)
-ax.set_ylabel('Binary Accuracy')
-ax.set_title('Binary Accuracy for train and validation')
-
-# Save artifacts
-# Plot
-plt.savefig(output_fig_path)
+#
+# # Build the model with the optimal hyperparameters
+# model = tuner.hypermodel.build(best_hps)
+#
+# # Find best epoch
+# history = model.fit(X_train_std,
+#                     Y_train,
+#                     batch_size=32,
+#                     validation_data=(X_val_std, Y_val),
+#                     class_weight={0: 1, 1: 2}, # Adjusting for the 67/33 split
+#                     epochs=100)
+#
+# val_acc_per_epoch = history.history['val_binary_accuracy']
+# best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+# print('Best epoch: %d' % (best_epoch,))
+#
+# # Show learned model with weights and biases from
+# W_final_layer, b_final_layer = model.layers[-1].get_weights()
+#
+# train_loss = history.history['loss'][-1]
+# train_binary_accuracy = history.history['binary_accuracy'][-1]
+# val_loss = history.history['val_loss'][-1]
+# val_binary_accuracy = history.history['val_binary_accuracy'][-1]
+#
+# print('Final train loss:', train_loss)
+# print('Final validation loss:', val_loss)
+# print('Final train_accuracy:', train_binary_accuracy)
+# print("Final_val_accuracy:", val_binary_accuracy)
+#
+# percentage_diff = ((train_loss - val_loss)/train_loss)*100
+# print(" percentage difference between the losses observed on the training and validation datasets:",percentage_diff )
+#
+# # grab history
+# history = history.history
+#
+# # plot loss for train and validation
+# fig = plt.figure(figsize=(16, 4))
+# ax = fig.add_subplot(1, 2, 1)
+# plt.plot(history['loss'], lw=2, color='darkgoldenrod')
+# plt.plot(history['val_loss'], lw=2, color='indianred')
+# plt.legend(['Train', 'Validation'], fontsize=10)
+# ax.set_xlabel('Epochs', size=10)
+# ax.set_ylabel('Loss')
+# ax.set_title('Loss for train and validation');
+#
+# # plot accuracy for train and validation
+# ax = fig.add_subplot(1, 2, 2)
+# plt.plot(history['binary_accuracy'], lw=2, color='darkgoldenrod')
+# plt.plot(history['val_binary_accuracy'], lw=2, color='indianred')
+# plt.legend(['Train', 'Validation'], fontsize=10)
+# ax.set_xlabel('Epochs', size=10)
+# ax.set_ylabel('Binary Accuracy')
+# ax.set_title('Binary Accuracy for train and validation')
+#
+# # Save artifacts
+# # Plot
+# plt.savefig(output_fig_path)
 
 
--- Model2_ffnn_best_model_eval.py
+++ Model2_ffnn_best_model_eval.py
@@ -16,8 +16,8 @@ from sklearn.metrics import precision_recall_curve, average_precision_score
 
 
 # Parameters
-analysis_df_path = '/Users/kleinjr1/Downloads/analysis_df.csv' # output from eda_and_preprocessing ruchit branch
-input_params_path = '/Users/kleinjr1/Downloads/best_hps.json'
+analysis_df_path = '/Users/kleinjr1/Downloads/analysis_df2.csv' # output from eda_and_preprocessing ruchit branch
+input_params_path = '/Users/kleinjr1/Downloads/best_hps2.json'
 
 # Load df
 df = pd.read_csv(analysis_df_path)
@@ -43,7 +43,7 @@ X_train, X_val, Y_train, Y_val = train_test_split(X_temporary, Y_temporary, test
 ############
 
 # Scale features
-numeric_cols = ['PRECIPITATION', 'MAX_TEMP', 'MIN_TEMP', 'AVG_WIND_SPEED', 'LAGGED_PRECIPITATION', 'LAGGED_AVG_WIND_SPEED']
+numeric_cols = ['PRECIPITATION', 'MAX_TEMP', 'MIN_TEMP', 'AVG_WIND_SPEED', 'LAGGED_PRECIPITATION', 'LAGGED_AVG_WIND_SPEED', 'DAYS_WITHOUT_RAIN']
 numeric_cols_lower = [i.lower() for i in numeric_cols]
 scaler = StandardScaler()
 
@@ -55,7 +55,7 @@ X_train_std[numeric_cols_lower] = scaler.fit_transform(X_train[numeric_cols_lowe
 X_val_std[numeric_cols_lower] = scaler.transform(X_val[numeric_cols_lower])
 X_test_std[numeric_cols_lower] = scaler.transform(X_test[numeric_cols_lower])
 
-# Recreate model
+# # Recreate model
 # model = model_builder(hp, (X_train.shape[1],))
 #
 #
@@ -66,16 +66,16 @@ X_test_std[numeric_cols_lower] = scaler.transform(X_test[numeric_cols_lower])
 #                     class_weight={0: 1, 1: 2}, # Adjusting for the 67/33 split
 #                     epochs=100)
 #
-# model.save("/Users/kleinjr1/Downloads/best_model.keras", save_format="tf")
+# model.save("/Users/kleinjr1/Downloads/best_model2.keras", save_format="tf")
 # history = history.history
-# with open("/Users/kleinjr1/Downloads/best_model_history.json", "w") as f:
-#     json.dump(history.history, f)
+# with open("/Users/kleinjr1/Downloads/best_model_history2.json", "w") as f:
+#     json.dump(history, f)
 
 # # Load saved model
-model = keras.models.load_model("/Users/kleinjr1/Downloads/best_model.keras")
+model = keras.models.load_model("/Users/kleinjr1/Downloads/best_model2.keras")
 
 # Load saved history
-with open("/Users/kleinjr1/Downloads/best_model_history.json", 'r') as file:
+with open("/Users/kleinjr1/Downloads/best_model_history2.json", 'r') as file:
     history = json.load(file)
 
 # Show learned model with weights and biases
@@ -102,7 +102,12 @@ val_accuracy = np.sum(final_model_val_accuracy_df['predictions'] == final_model_
 test_accuracy = np.sum(final_model_test_accuracy_df['predictions'] == final_model_test_accuracy_df['actuals']) / len(final_model_test_accuracy_df)
 
 agg_accuracy = np.sum(agg_accuracy_df['predictions'] == agg_accuracy_df['actuals']) / len(agg_accuracy_df)
-
+print('train accurayc')
+print(train_accuracy)
+print('test accuracy')
+print(test_accuracy)
+print('val_accuracy')
+print(val_accuracy)
 # Optional to get accuracies from model.evaluate as well (returned as 2nd param given how model_builder is set up)
 print("Gap between aggregate test and train accuracy", test_accuracy - train_accuracy)
 
@@ -127,7 +132,7 @@ print(classification_report(
 
 
 # Training Process diagnostics
-output_fig_path = '/Users/kleinjr1/Downloads/results.png'
+output_fig_path = '/Users/kleinjr1/Downloads/results2.png'
 
 val_acc_per_epoch = history['val_binary_accuracy']
 best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
