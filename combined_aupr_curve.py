8ffe0a5d438f303ea9aeb0001a71bed25bc5d98f
kleinjr1 <kleinjr1@berkeley.edu>
2026-04-05 15:45:07 -0400

    auc curve

--- combined_aupr_curve.py
+++ combined_aupr_curve.py
@@ -14,14 +14,34 @@ model_results = {'FFNN': ffnn_results,
 
 fig, ax = plt.subplots()
 
+# # Precision Recall Curve
+# for model_name, df in model_results.items():
+#     labels, preds = df['true_labels'], df['probabilities']
+#     # P-R Curve
+#     precision, recall, _ = metrics.precision_recall_curve(labels, preds)
+#     avg = metrics.average_precision_score(labels, preds)
+#     ax.plot(recall, precision, label=f"{model_name} (Avg Prec: {avg:.3f})")
+#
+# ax.legend()
+# ax.set_title("Precision-Recall for all models")
+# ax.set_xlabel("Recall")
+# ax.set_ylabel("Precision")
+# plt.show()
+
+# Area Under ROC Curve
 for model_name, df in model_results.items():
     labels, preds = df['true_labels'], df['probabilities']
-    precision, recall, _ = metrics.precision_recall_curve(labels, preds)
-    avg = metrics.average_precision_score(labels, preds)
-    ax.plot(recall, precision, label=f"{model_name} (Avg Prec: {avg:.3f})")
+    fpr, tpr, _ = metrics.roc_curve(labels, preds)
+    auc = metrics.auc(fpr, tpr)
+    ax.plot(fpr, tpr, label=f"{model_name} (AUC: {auc:.3f})")
 
-ax.legend()
-ax.set_title("Precision-Recall for all models")
-ax.set_xlabel("Recall")
-ax.set_ylabel("Precision")
+ax.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--', label='Reference')
+ax.set_xlim(0.0, 1.0)
+ax.set_ylim(0.0, 1.05)
+ax.set_xlabel('False Positive Rate (1 - Specificity)')
+ax.set_ylabel('True Positive Rate (Sensitivity/Recall)')
+ax.set_title('ROC Curve for Fire Detection')
+ax.legend(loc="lower right")
+ax.grid(alpha=0.3)
+
 plt.show()
